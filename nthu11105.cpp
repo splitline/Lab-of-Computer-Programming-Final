6b23dfbb0e9848fd7e9da12ec08b57ebe4110aa3
黃志仁 <tbsthitw@gmail.com>
2016-12-26 12:52:57 +0800

    UVa 12694

---
+++ nthu11105.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	long long int t,in,n;
+	scanf("%lld",&t);
+	for(int i=1;i<=t;i++){
+		scanf("%lld",&in);
+		n=0;
+
+	}
+}
--- uva12694.cpp
+++ uva12694.cpp
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+struct N{
+	int s,f;
+};
+int cmp (const void * a, const void * b){
+	return  (*(N *)a).s > (*(N *)b).s ? -1 : 1;
+}
 int main(){
 	int t;
 	scanf("%d",&t);
-	while(t--){
-		
+	N in[25];
+	int ind=0;
+	while(~scanf("%d%d",&in[ind].s,&in[ind].f)){
+		if(in[ind].s+in[ind].f==0){
+			int time[10]={0},add[25]={0},max=0;
+			qsort(in,ind,sizeof(in[0]),cmp);
+			for(int i=0;i<ind;i++){
+				for(int j=in[i].s;j<in[i].f;j++){
+					if(time[j])break;
+					if(j==in[i].f-1){
+						for(int k=in[i].s;k<in[i].f;k++)time[k]=1;
+						max++;
+					}
+				}
+			}
+			printf("%d\n",max);
+			ind=0;
+		}
+		else ++ind;
 	}
 }
